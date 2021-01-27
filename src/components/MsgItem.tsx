d6fed2027e85dbed3954c6e8285d0b98e6140957
Jayeeson <randomness363@hotmail.com>
2021-01-26 21:22:19 -0500

    MsgItem broken out to component

---
+++ src/components/MsgItem.tsx
@@ -0,0 +1,51 @@
+import { Avatar, ListItem, ListItemAvatar, ListItemText, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+import { connect } from 'react-redux';
+import { RootState } from '../store';
+import { ChatMessage, ChatUser } from '../types';
+
+const useStyles = makeStyles({
+  timestamp: {
+    paddingLeft: '1rem',
+  },
+});
+
+const MsgItem = ({ message, users }: { message: ChatMessage; users: ChatUser[] }) => {
+  const displayName = users.find(user => user.id === message.userId)?.displayName;
+  const classes = useStyles();
+
+  return (
+    <ListItem key={message.id}>
+      <ListItemAvatar>
+        <Avatar variant="rounded">{displayName?.length ? displayName.slice(0, 1).toLocaleUpperCase() : '?'}</Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        disableTypography
+        primary={
+          <div>
+            <Typography variant="h6" component="span">
+              {displayName}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="span" className={classes.timestamp}>
+              {new Date(message.timestamp).toLocaleTimeString().split(/(?<=\d+:\d{2}):\d\d/)}
+            </Typography>
+          </div>
+        }
+        secondary={
+          <Typography variant="body1" color="textPrimary">
+            {message.content}
+          </Typography>
+        }
+      />
+    </ListItem>
+  );
+};
+
+const mapStateToProps = (state: RootState) => {
+  return {
+    users: Object.values(state.chat.users),
+  };
+};
+
+export default connect(mapStateToProps)(MsgItem);
--- src/components/MsgList.tsx
+++ src/components/MsgList.tsx
@@ -1,29 +1,14 @@
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { List } from '@material-ui/core';
 import React, { createRef, useEffect, useRef, useState } from 'react';
 import { connect } from 'react-redux';
 import { RootState } from '../store';
-import { ChatChannel, ChatMessage, ChatUser } from '../types';
+import { ChatChannel, ChatMessage } from '../types';
 import IMsgPanel from './IMsgPanel';
+import MsgItem from './MsgItem';
 
-const useStyles = makeStyles({
-  timestamp: {
-    paddingLeft: '1rem',
-  },
-});
-
-const MsgList = ({
-  messages,
-  users,
-  activeChannel,
-}: {
-  messages: ChatMessage[];
-  users: ChatUser[];
-  activeChannel: ChatChannel | undefined;
-}) => {
+const MsgList = ({ messages, activeChannel }: { messages: ChatMessage[]; activeChannel: ChatChannel | undefined }) => {
   const [listHeightBeforeNewMessageAdded, setListHeightBeforeNewMessageAdded] = useState(0);
 
-  const classes = useStyles();
   const containerRef = createRef<HTMLDivElement>();
   const listRef = useRef<HTMLUListElement>(null);
 
@@ -32,7 +17,7 @@ const MsgList = ({
       const { height: containerHeight } = containerRef.current.getBoundingClientRect();
       const { height: listHeight } = listRef.current.getBoundingClientRect();
       const { scrollTop } = containerRef.current;
-      const tolerance = 12; //px
+      const tolerance = 12; //px, defines zone where autoscroll occurs on new message
       if (
         scrollTop + tolerance >= listHeightBeforeNewMessageAdded - containerHeight &&
         listHeightBeforeNewMessageAdded > 100
@@ -48,39 +33,14 @@ const MsgList = ({
     }
   }, [messages]);
 
-  const renderMessageItem = (message: ChatMessage) => {
-    const displayName = users.find(user => user.id === message.userId)?.displayName;
-    return (
-      <ListItem key={message.id}>
-        <ListItemAvatar>
-          <Avatar variant="rounded">{displayName?.length ? displayName.slice(0, 1).toLocaleUpperCase() : '?'}</Avatar>
-        </ListItemAvatar>
-        <ListItemText
-          disableTypography
-          primary={
-            <div>
-              <Typography variant="h6" component="span">
-                {displayName}
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="span" className={classes.timestamp}>
-                {new Date(message.timestamp).toLocaleTimeString().split(/(?<=\d+:\d{2}):\d\d/)}
-              </Typography>
-            </div>
-          }
-          secondary={
-            <Typography variant="body1" color="textPrimary">
-              {message.content}
-            </Typography>
-          }
-        />
-      </ListItem>
-    );
+  const renderMessageList = () => {
+    return messages.map(message => <MsgItem key={message.id} message={message} />);
   };
 
   return (
     <IMsgPanel header={`#${activeChannel?.name}`} setRef={containerRef}>
       <List id="messageList" ref={listRef}>
-        {messages.map(renderMessageItem)}
+        {renderMessageList()}
       </List>
     </IMsgPanel>
   );
@@ -89,7 +49,6 @@ const MsgList = ({
 const mapStateToProps = (state: RootState) => {
   return {
     messages: Object.values(state.chat.messages).filter(message => message.channelId === state.chat.activeChannelId),
-    users: Object.values(state.chat.users),
     activeChannel: Object.values(state.chat.channels).find(channel => channel.id === state.chat.activeChannelId),
   };
 };
